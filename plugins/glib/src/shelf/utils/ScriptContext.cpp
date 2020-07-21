e47eebc0b19fa385faee3125619112577e07d71d
gsioteam <gsioteam@protonmail.com>
2020-07-21 10:58:11 +0800

    added picture_viewer script

---
+++ plugins/glib/src/shelf/utils/ScriptContext.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gen on 7/21/2020.
+//
+
+#include "ScriptContext.h"
---
+++ plugins/glib/src/shelf/utils/ScriptContext.h
@@ -0,0 +1,16 @@
+//
+// Created by gen on 7/21/2020.
+//
+
+#ifndef ANDROID_SCRIPTCONTEXT_H
+#define ANDROID_SCRIPTCONTEXT_H
+
+
+
+class ScriptContext {
+
+};
+
+
+
+#endif //ANDROID_SCRIPTCONTEXT_H
