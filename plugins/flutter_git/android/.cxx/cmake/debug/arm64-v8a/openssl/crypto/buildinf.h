ad816c75f10532f30b924267948132705041bc3b
gsio <gsio@protonmail.com>
2021-11-22 13:40:38 +0700

    Remove unnecessary object

--- plugins/flutter_git/android/.cxx/cmake/debug/arm64-v8a/openssl/crypto/buildinf.h
+++
@@ -1,6 +0,0 @@
-/* auto-generated for crypto/cversion.c */
-#define CFLAGS "compiler: Clang -g -DANDROID -fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security "
-#define PLATFORM "platform: Android"
-#define DATE "built on: 2021-11-21 09:54:57 UTC"
-static const char *compiler_flags = CFLAGS;
-
